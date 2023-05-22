ed6c606ae85d9ab82d201de86920efdc0d353adb
Narisong Wang <narisong.wang@gmail.com>
2023-05-23 07:54:25 +1000

    updated app icon and splash back

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('Useless Text');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setText}
+        value={text}
+      ></TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +25,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    height: 300,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
