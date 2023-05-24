a795185ec2d3436a709c1b8182c46c55035c5f00
Narisong Wang <narisong.wang@gmail.com>
2023-05-24 11:45:24 +1000

    added and configured nativewind and tailwind css, configured .env

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontSize: {
+        xxs: ['10px', '14px'],
+      },
+    },
+  },
+  plugins: [],
+};
